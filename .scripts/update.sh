2e6b22621e5dbf7eef2b18c637f6c1895f453f16
Serge K <cmt@phnx47.net>
2025-04-26 14:27:09 +0700

    delete srcname from update script

--- .scripts/update.sh
+++ .scripts/update.sh
@@ -3,18 +3,16 @@
 set -eu -o pipefail
 
 pkgname="${1%/}"
-srcname="${pkgname%-bin}"
 
 echo "pkgname=${pkgname}"
-echo "srcname=${srcname}"
 
 if [ -z "${pkgname}" ]; then
   echo "Please provide pkgname!"
   exit 1
 fi
 
-nvchecker -e "${srcname}" -c nvchecker.toml
-nver=$(jq -r .data.\""${srcname}"\".version nver.json)
+nvchecker -e "${pkgname}" -c nvchecker.toml
+nver=$(jq -r .data.\""${pkgname}"\".version nver.json)
 sed -E -i "s/pkgver=.*/pkgver=${nver}/" "${pkgname}/PKGBUILD"
 sed -E -i "s/pkgrel=.*/pkgrel=1/" "${pkgname}/PKGBUILD"
 
@@ -26,6 +24,6 @@ git add PKGBUILD .SRCINFO
 git commit -m "v${nver}"
 cd ..
 
-nvtake "${srcname}" -c nvchecker.toml
+nvtake "${pkgname}" -c nvchecker.toml
 git add "${pkgname}" ver.json
-git commit -m "${srcname}: v${nver}"
+git commit -m "${pkgname}: v${nver}"
