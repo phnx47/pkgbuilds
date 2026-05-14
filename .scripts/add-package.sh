1b52de908ba5d75f640b25e2f65b70c5cd28c991
Serge K <cmt@phnx47.net>
2026-05-14 18:05:52 +0700

    swap add/remove scripts

--- .scripts/add-package.sh
+++ .scripts/add-package.sh
@@ -6,7 +6,16 @@ cd "$(dirname "$0")/.."
 
 pkgname="${1}"
 
-git rm -f "${pkgname}"
-rm -rf ".git/modules/${pkgname}"
-git config --remove-section "submodule.${pkgname}"
-git commit -m "remove ${pkgname} pkg"
+git submodule add -b master -f "ssh://aur@aur.archlinux.org/${pkgname}.git"
+git config -f .gitmodules "submodule.${pkgname}.ignore" untracked
+
+# sort .gitmodules
+awk 'BEGIN { I=0 ; J=0 ; K="" } ; /^\[submodule/{ N+=1 ; J=1 ; K=$2 ; gsub(/("vendor\/|["\]])/, "", K) } ; { print K, N, J, $0 } ; { J+=1 }' .gitmodules |
+  sort |
+  awk '{ $1="" ; $2="" ; $3="" ; print }' |
+  sed 's/^ *//g' |
+  awk '/^\[/{ print ; next } { print "\t" $0 }' >.gitmodules.tmp
+mv .gitmodules.tmp .gitmodules
+
+git add .gitmodules
+git commit -m "add ${pkgname} pkg"
